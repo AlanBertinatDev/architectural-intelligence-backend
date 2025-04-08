fca82b0dc5f3f7170bb8869a3989eb483de7c3a9
Alan Bertinat <alanbertinatestudio@gmail.com>
2025-04-08 17:40:47 -0300

    Proyecto Spring Boot inicial

---
+++ backend-architecture/src/main/java/com/architecture/backend_architecture/BackendArchitectureApplication.java
@@ -0,0 +1,13 @@
+package com.architecture.backend_architecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendArchitectureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendArchitectureApplication.class, args);
+	}
+
+}
